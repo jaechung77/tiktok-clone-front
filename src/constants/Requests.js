e8573c56fe0a0107b2dc7336d6aae06d9c082dab
Jae Chung <jaechung77@gmail.com>
2022-05-05 15:12:08 +1000

    change Request Constant File to Heroku

--- src/constants/Requests.js
+++ src/constants/Requests.js
@@ -1,6 +1,6 @@
-// const API = "https://quiet-plains-45335.herokuapp.com"
-const API = "http://localhost:3000"
-const S3 = "http://localhost:3000"
+const API = "https://quiet-plains-45335.herokuapp.com"
+//const API = "http://localhost:3000"
+const S3 = "https://tt-clone-api-v1-bucket.storage.googleapis.com/uploads/post/file"
 const requests = {
   API: `${API}`,
   fetchVideos:`${API}/posts`,
